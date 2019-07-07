3c8a3b516320107c5b3cf73992fe0550a01e7fc6
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-07 08:16:06 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,6 @@ class Song < ActiveRecord::Base
 
   def to_s
     #{}"#{Song.artist.name} - #{Song.title}"
-    self.artist.name + '-' + self.title
+    self.artist.name + ' - ' + self.title
   end
 end
