661a9149f69e12c5d1050bea49471d6c44039579
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-07-07 08:08:59 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def to_s
-    "#{Song.artist.name} - #{Song.title}"
+    #{}"#{Song.artist.name} - #{Song.title}"
+    Song.artist.name + '-' + Song.title
   end
 end
